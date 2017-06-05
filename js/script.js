d06d1dda7fd0bbcea30a91a8d8b316a8c09a799a
tlazarus88 <trevor.das.lazarus@gmail.com>
2017-06-04 22:42:27 -0400

    Styled Framework
    + Fixed Bouncing Arrow
    + Styled Sections

--- js/script.js
+++ js/script.js
@@ -8,22 +8,28 @@ $(function(){
 			if ($(document).scrollTop() >= $("#intro").position().top) {
 					$('body').css('background', $("#intro").attr("data-color"));
 					$('nav').css('background', $("#intro").attr("data-color"));
+					$('#down-label').text('');
 			};
 			if ($(document).scrollTop() > $("#now").position().top) {
 					$('body').css('background', $("#now").attr("data-color"))
 					$('nav').css('background', $("#now").attr("data-color"));
+					$('#down-label').text('Then');
 			};
 			if ($(document).scrollTop() > $("#then").position().top) {
 					$('body').css('background', $("#then").attr("data-color"))
 					$('nav').css('background', $("#then").attr("data-color"));
+					$('#down-label').text('About Me');
 			};
 			if ($(document).scrollTop() > $("#about-me").position().top) {
 					$('body').css('background', $("#about-me").attr("data-color"))
 					$('nav').css('background', $("#about-me").attr("data-color"));
+					$('#down-label').text('Connect');
+					$('#down-directional').css('visibility', 'visible');
 			};
 			if ($(document).scrollTop() >= $("#connect").position().top) {
 					$('body').css('background', $("#connect").attr("data-color"))
 					$('nav').css('background', $("#connect").attr("data-color"));
+					$('#down-directional').css('visibility', 'hidden');
 			};
 		});
   	}
