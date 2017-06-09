3d1b9dc5d4cf48489d24f39cdb71ff80448c4657
tlazarus88 <trevor.das.lazarus@gmail.com>
2017-06-08 22:31:26 -0400

    Installed Fullpage Plugin
    And integrated it with section css transitions

--- js/script.js
+++ js/script.js
@@ -2,37 +2,60 @@
 
 jQuery(document).ready(function($) {
 
-$(function(){
-  	if($('body').is('.home')){
-	  	$(window).on("scroll touchmove", function() {
-			if ($(document).scrollTop() >= $("#intro").position().top) {
-					$('body').css('background', $("#intro").attr("data-color"));
-					$('nav').css('background', $("#intro").attr("data-color"));
-					$('#down-label').text('');
-			};
-			if ($(document).scrollTop() > $("#now").position().top) {
-					$('body').css('background', $("#now").attr("data-color"))
-					$('nav').css('background', $("#now").attr("data-color"));
-					$('#down-label').text('Then');
-			};
-			if ($(document).scrollTop() > $("#then").position().top) {
-					$('body').css('background', $("#then").attr("data-color"))
-					$('nav').css('background', $("#then").attr("data-color"));
-					$('#down-label').text('About Me');
-			};
-			if ($(document).scrollTop() > $("#about-me").position().top) {
-					$('body').css('background', $("#about-me").attr("data-color"))
-					$('nav').css('background', $("#about-me").attr("data-color"));
-					$('#down-label').text('Connect');
-					$('#down-directional').css('visibility', 'visible');
-			};
-			if ($(document).scrollTop() >= $("#connect").position().top) {
-					$('body').css('background', $("#connect").attr("data-color"))
-					$('nav').css('background', $("#connect").attr("data-color"));
-					$('#down-directional').css('visibility', 'hidden');
-			};
-		});
-  	}
-});	
+// Call Fullpage Plugin
+$('#fullpage').fullpage({
+
+	// Background Color Transition
+	onLeave: function(index, nextIndex, direction){
+		var leavingSection = $(this);
+
+		if(index == 1 && direction =='down'){
+			$('body').css('background', $("#now").attr("data-color"))
+			$('nav').css('background', $("#now").attr("data-color"));
+			$('#down-label').text('Then');
+		};
+
+		if(index == 2 && direction =='down'){
+			$('body').css('background', $("#then").attr("data-color"))
+			$('nav').css('background', $("#then").attr("data-color"));
+			$('#down-label').text('About Me')
+		}
+		else if(index == 2 && direction == 'up'){
+			$('body').css('background', $("#intro").attr("data-color"));
+			$('nav').css('background', $("#intro").attr("data-color"));
+			$('#down-label').text('');
+		};
+
+		if(index == 3 && direction =='down'){
+			$('body').css('background', $("#about-me").attr("data-color"))
+			$('nav').css('background', $("#about-me").attr("data-color"));
+			$('#down-label').text('Connect');
+		}
+		else if(index == 3 && direction == 'up'){
+			$('body').css('background', $("#now").attr("data-color"))
+			$('nav').css('background', $("#now").attr("data-color"));
+			$('#down-label').text('Then');
+		};
+
+		if(index == 4 && direction =='down'){
+			$('body').css('background', $("#connect").attr("data-color"))
+			$('nav').css('background', $("#connect").attr("data-color"));
+			$('#down-directional').css('visibility', 'hidden');
+		}
+		else if(index == 4 && direction == 'up'){
+			$('body').css('background', $("#then").attr("data-color"))
+			$('nav').css('background', $("#then").attr("data-color"));
+			$('#down-label').text('About Me');
+		};
+
+		if(index == 5 && direction == 'up'){
+			$('body').css('background', $("#about-me").attr("data-color"))
+			$('nav').css('background', $("#about-me").attr("data-color"));
+			$('#down-label').text('Connect');
+			$('#down-directional').css('visibility', 'visible');
+		};
+
+	}
+});
 
 });
