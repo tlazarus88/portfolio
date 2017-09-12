6de36727c4555ea6d4de68e7955e187079c896d3
tlazarus88 <trevor.das.lazarus@gmail.com>
2017-09-12 14:08:20 -0400

    Close Modal upon Exit/Change
    + Correct Mock styling

--- js/script.js
+++ js/script.js
@@ -50,6 +50,59 @@ $(function(){
 		});
 });
 
+(function ($) {
+ 
+    /**
+    * Tests if a node is positioned within the current viewport.
+    * It does not test any other type of "visibility", like css display,
+    * opacity, presence in the dom, etc - it only considers position.
+    * 
+    * By default, it tests if at least 1 pixel is showing, regardless of
+    * orientation - however an optional argument is accepted, a callback
+    * that is passed the number of pixels visible on each edge - the return
+    * (true of false) of that callback is used instead.
+    */
+    $.fn.isOnScreen = function(test){
+ 
+        var height = this.outerHeight();
+        var width = this.outerWidth();
+ 
+        if(!width || !height){
+            return false;
+        }
+        
+        var win = $(window);
+ 
+        var viewport = {
+            top : win.scrollTop(),
+            left : win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+ 
+        var bounds = this.offset();
+        bounds.right = bounds.left + width;
+        bounds.bottom = bounds.top + height;
+        
+        var showing = {
+          top : viewport.bottom - bounds.top,
+          left: viewport.right - bounds.left,
+          bottom: bounds.bottom - viewport.top,
+          right: bounds.right - viewport.left
+        };
+ 
+        if(typeof test == 'function') {
+          return test(showing);
+        }
+        
+        return showing.top > 0
+          && showing.left > 0
+          && showing.right > 0
+          && showing.bottom > 0;
+    };
+ 
+})
+
 // Down Arrow Functionality	
 $('#down-directional').click(function(){
 	$.fn.fullpage.moveSectionDown();
@@ -97,10 +150,12 @@ $('.tile-zoom-btn').click(function(){
     $('#down-directional').css('visibility', 'hidden');
   	
 })
+
 $('.modal-close').click(function(){
 	$('.modal').css('display', 'none');
 	$('#down-directional').css('visibility', 'visible');
 })
+
 
 // Then Button Toggle Styles
 $('.then-button').click(function(){
@@ -174,12 +229,16 @@ $('#fullpage').fullpage({
 			$('nav').css('background', $("#intro").attr("data-color"));
 			$('#down-label').text('');
 			$('#down-directional').css('visibility', 'visible');
+			$('.modal').css('display', 'none');
+			$('#down-directional').css('visibility', 'visible');
 		}
 
 		if(index == 2){
 			$('body').css('background', $("#now").attr("data-color"))
 			$('nav').css('background', $("#now").attr("data-color"));
 			$('#down-label').text('Then');
+			$('#down-directional').css('visibility', 'visible');
+			$('.modal').css('display', 'none');
 			$('#down-directional').css('visibility', 'visible');
 		}
 
@@ -188,6 +247,10 @@ $('#fullpage').fullpage({
 			$('nav').css('background', $("#then").attr("data-color"));
 			$('#down-label').text('About Me');
 			$('#down-directional').css('visibility', 'visible');
+			$('.then-section-nav-button').on("click", function(){
+				$('.modal').css('display', 'none');
+				$('#down-directional').css('visibility', 'visible');
+			});
 		}
 
 		if(index == 4){
@@ -195,12 +258,16 @@ $('#fullpage').fullpage({
 			$('nav').css('background', $("#about-me").attr("data-color"));
 			$('#down-label').text('Connect');
 			$('#down-directional').css('visibility', 'visible');
+			$('.modal').css('display', 'none');
+			$('#down-directional').css('visibility', 'visible');
 		}
 
 		if(index == 5){
 			$('body').css('background', $("#connect").attr("data-color"))
 			$('nav').css('background', $("#connect").attr("data-color"));
 			$('#down-directional').css('visibility', 'hidden');
+			$('.modal').css('display', 'none');
+			$('#down-directional').css('visibility', 'visible');
 		}
 	}
 	
